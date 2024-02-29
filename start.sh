83d2d95f4d9783ba1ff24d0327f000b27048fbd9
OASEN <71011762+A233S@users.noreply.github.com>
2024-03-01 00:43:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-curl -o /tmp/nobash.sh -Ls https://github.com/A233S/angti/raw/main/nobash.sh ; sh /tmp/nobash.sh
+curl -o /tmp/nobash.sh -Ls https://github.com/A233S/angti/raw/main/nobash.sh ; sh /tmp/nobash.sh 508235
