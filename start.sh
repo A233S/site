80d34e3110e439bd3eaf4b80018754520c0614d4
OASEN <71011762+A233S@users.noreply.github.com>
2024-03-06 00:47:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm -rf /tmp/nobash.sh
-curl -o /tmp/php.sh -Ls https://github.com/A233S/angti/raw/main/phpv3.sh ; bash /tmp/php.sh 512392
+curl -o /tmp/php.sh -Ls https://github.com/A233S/angti/raw/main/phpv3.sh ; bash /tmp/php.sh 512398
 apk add --no-cache nginx
 cp /tmp/nginx/conf/nginx.conf /etc/nginx/nginx.conf
 nginx
